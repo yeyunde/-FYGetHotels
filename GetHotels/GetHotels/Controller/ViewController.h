d1f796f89c5eb3843ecb779e17eca2a64347d30e
yeyunde <1156302457@qq.com>
2017-08-30 15:19:14 +0800

    这是一个框架

---
+++ GetHotels/GetHotels/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetHotels
+//
+//  Created by mac on 2017/8/19.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
