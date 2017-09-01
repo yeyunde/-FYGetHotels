e7a10add38467e38915b472765a5763b7c5a5005
Wu Zhi Ren <1528504616@qq.com>
2017-09-01 11:59:57 +0800

    我的

---
+++ GetHotels/GetHotels/Controller/MyInfoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MyInfoTableViewCell.m
+//  GetHotels
+//
+//  Created by admin on 2017/9/1.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import "MyInfoTableViewCell.h"
+
+@implementation MyInfoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
